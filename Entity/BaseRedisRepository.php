e5f45d0a8750a15fa20be273957cd18309fc4f5a
Alex Tabaksmann <at@virtualnights.com>
2012-10-29 13:23:07 +0100

    expire added

--- Entity/BaseRedisRepository.php
+++ Entity/BaseRedisRepository.php
@@ -29,6 +29,15 @@ class BaseRedisRepository
     {
         return $this->redis;
     }
+	
+    /**
+     * Will delete given $key after $ttl seconds
+     *
+     */
+    public function expire($key, $ttl)
+    {
+        $this->redis->expire($key, $ttl);
+    }	
 
     /**
      * Will get a RedisEntity that matches the given key. The Key can be the BaseKey or a Key with values.
